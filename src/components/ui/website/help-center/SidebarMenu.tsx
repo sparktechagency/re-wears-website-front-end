41db4ca1cf7f4be296d92d47a9fb6bbc6c6c1ac4
Rahad Ullah <myemail.abc20@gmail.com>
2025-03-10 08:41:11 +0600

    Added getting started page and modified sidebar menus

--- src/components/ui/website/help-center/SidebarMenu.tsx
+++ src/components/ui/website/help-center/SidebarMenu.tsx
@@ -1,75 +1,32 @@
+"use client";
+
+import { sidebarMenus } from "@/constants/help-center/sidebarMenu";
 import { Divider } from "antd";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const SidebarMenu = () => {
+  const pathname = usePathname().split("/").filter(Boolean)[length + 1];
+  console.log(pathname);
+
   return (
     <>
       <h1 className="heading-1 !pt-2">Help Center</h1>
       <ul>
-        <li className="h-auto">
-          <Link href={`/help-center`}>
-            <button className="text-[#797979] py-3 focus:font-bold w-full text-start">
-              Home
-            </button>
-          </Link>
-          <Divider style={{ margin: "0" }} />
-        </li>
-        <li className="h-auto">
-          <Link href={`/help-center/getting-started`}>
-            <button className="text-[#797979] py-3 focus:font-bold w-full text-start">
-              Getting Started
-            </button>
-          </Link>
-          <Divider style={{ margin: "0" }} />
-        </li>
-        <li className="h-auto">
-          <Link href={`/help-center/selling`}>
-            <button className="text-[#797979] py-3 focus:font-bold w-full text-start">
-              Selling
-            </button>
-          </Link>
-          <Divider style={{ margin: "0" }} />
-        </li>
-        <li className="h-auto">
-          <Link href={`/help-center/buying`}>
-            <button className="text-[#797979] py-3 focus:font-bold w-full text-start">
-              Buying
-            </button>
-          </Link>
-          <Divider style={{ margin: "0" }} />
-        </li>
-        <li className="h-auto">
-          <Link href={`/help-center/shipping`}>
-            <button className="text-[#797979] py-3 focus:font-bold w-full text-start">
-              Shipping
-            </button>
-          </Link>
-          <Divider style={{ margin: "0" }} />
-        </li>
-        <li className="h-auto">
-          <Link href={`/help-center/payments`}>
-            <button className="text-[#797979] py-3 focus:font-bold w-full text-start">
-              Payments
-            </button>
-          </Link>
-          <Divider style={{ margin: "0" }} />
-        </li>
-        <li className="h-auto">
-          <Link href={`/help-center/safety-&-reporting`}>
-            <button className="text-[#797979] py-3 focus:font-bold w-full text-start">
-              Safety & Reporting
-            </button>
-          </Link>
-          <Divider style={{ margin: "0" }} />
-        </li>
-        <li className="h-auto">
-          <Link href={`/help-center/my-account-&-settings`}>
-            <button className="text-[#797979] py-3 focus:font-bold w-full text-start">
-              My account & Settings
-            </button>
-          </Link>
-          <Divider style={{ margin: "0" }} />
-        </li>
+        {sidebarMenus.map((item, idx) => (
+          <li key={idx} className="h-auto">
+            <Link href={`/help-center/${item.path}`}>
+              <button
+                className={`text-[#797979] py-3 w-full text-start hover:text-black ${
+                  pathname === item.path && "font-bold text-black"
+                }`}
+              >
+                {item.title}
+              </button>
+            </Link>
+            <Divider style={{ margin: "0" }} />
+          </li>
+        ))}
       </ul>
     </>
   );
---
+++ src/constants/help-center/sidebarMenu.ts
@@ -0,0 +1,34 @@
+export const sidebarMenus = [
+  {
+    title: "Home",
+    path: "home",
+  },
+  {
+    title: "Getting Started",
+    path: "getting-started",
+  },
+  {
+    title: "Selling",
+    path: "selling",
+  },
+  {
+    title: "Buying",
+    path: "buying",
+  },
+  {
+    title: "Shipping",
+    path: "shipping",
+  },
+  {
+    title: "Payments",
+    path: "payments",
+  },
+  {
+    title: "Safety & Reporting",
+    path: "safety-&-reporting",
+  },
+  {
+    title: "My account & Settings",
+    path: "my-account-&-settings",
+  },
+];
