29e1ac6b63dba240d5a72eaf49c9382c38dbdfff
Rahad Ullah <myemail.abc20@gmail.com>
2025-03-10 10:33:11 +0600

    Added buying, shipping page

---
+++ src/app/(website)/help-center/buying/page.tsx
@@ -0,0 +1,24 @@
+const BuyingPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl lg:text-3xl font-bold py-4">Buying made easy</h1>
+
+      {/* content section */}
+      <section className="grid gap-6 py-4">
+        <p>
+          Shipping arrangements are made at the discretion and agreement between
+          the buyer and seller. Once you&apos;ve completed your sale, please
+          communicate with the buyer to discuss shipping options.{" "}
+        </p>
+        <p>
+          Currently, re-wears does not provide delivery services. However, we
+          are actively working on implementing this feature in the near future.
+          Please bear with us as we strive to enhance your experience on our
+          platform.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default BuyingPage;
