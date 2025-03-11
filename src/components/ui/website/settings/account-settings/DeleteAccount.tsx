fa663a4c0fd36863cd7c127e4dfa5a6bca965a24
Rahad Ullah <myemail.abc20@gmail.com>
2025-03-11 08:10:05 +0600

    Added delete account page

---
+++ src/components/ui/website/settings/account-settings/DeleteAccount.tsx
@@ -0,0 +1,46 @@
+import FillButton from "@/components/shared/FillButton";
+import Label from "@/components/shared/Label";
+import TextArea from "@/components/shared/TextArea";
+import { Checkbox } from "antd";
+import Link from "next/link";
+
+const DeleteAccount = () => {
+  return (
+    <div className="grid gap-5">
+      <section className="card grid-between">
+        <div>
+          <p className="text-[#797979] text-sm">Delete my account</p>
+          <Label>Help us improve</Label>
+        </div>
+        <TextArea placeholder="Tell us why you're closing your account" />
+      </section>
+
+      <section className="card grid-between">
+        <Label>I confirm that all my orders are completed</Label>
+        <div className="flex lg:justify-end items-center">
+          <Checkbox className="scale-150" />
+        </div>
+      </section>
+
+      <section>
+        <p className="text-sm text-[#797979]">
+          If you delete your account, it will be deactivated immediately.
+          Deactivated accounts are only visible to re-wears team before they are
+          permanently deleted. The deletion takes place within the time frames
+          indicated in re-wears&apos;s{" "}
+          <Link href={""} className="link">
+            Privacy Policy.
+          </Link>
+        </p>
+      </section>
+
+      <section className="flex justify-end">
+        <FillButton className="bg-[#D04555] hover:bg-[#a32937] uppercase">
+          Delete account
+        </FillButton>
+      </section>
+    </div>
+  );
+};
+
+export default DeleteAccount;
