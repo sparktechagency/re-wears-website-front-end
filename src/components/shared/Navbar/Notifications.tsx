342280caf12aef41f61ff91a184436dac33461f0
Rahad Ullah <myemail.abc20@gmail.com>
2025-03-11 13:48:44 +0600

    Navbar modified

---
+++ src/components/shared/Navbar/Notifications.tsx
@@ -0,0 +1,83 @@
+import icon from "@/assets/icons/bell-notification.svg";
+import Image from "next/image";
+import Label from "../Label";
+const notifications = [
+  {
+    username: "John Doe",
+    photo: "https://korika.id/wp-content/uploads/2017/10/speaker3-min.jpg",
+    items: [
+      "Flower sunglasses for girl 3/7 years",
+      "Maileg first kit box",
+      "The Kooples short printed floral lavender dress",
+    ],
+    time: "1 day",
+  },
+  {
+    username: "John Smith",
+    photo: "https://korika.id/wp-content/uploads/2017/10/speaker3-min.jpg",
+    items: [
+      "Flower sunglasses for girl 3/7 years",
+      "Maileg first kit box",
+      "The Kooples short printed floral lavender dress",
+    ],
+    time: "2 day",
+  },
+  {
+    username: "Xein Alex",
+    photo: "https://korika.id/wp-content/uploads/2017/10/speaker3-min.jpg",
+    items: [
+      "Flower sunglasses for girl 3/7 years",
+      "Maileg first kit box",
+      "The Kooples short printed floral lavender dress",
+    ],
+    time: "3 day",
+  },
+];
+const Notifications = () => {
+  return (
+    <section className="max-w-[350px] max-h-[500px] font-poppins">
+      {/* show empty page when no notification found */}
+      {notifications.length < 1 && (
+        <div className="flex flex-col items-center p-8 ">
+          <Image src={icon} alt="icon" width={45} height={53} />
+          <Label className="text-lg">No notifications yet</Label>
+          <p className="text-[#797979]">
+            This is where you&apos;ll find your notifications.
+          </p>
+        </div>
+      )}
+
+      {/* display when notification found */}
+      <div>
+        <ul>
+          {notifications.map((item, idx) => (
+            <li key={idx} className="flex gap-4 py-2 border-b">
+              <Image
+                src={item.photo}
+                alt="photo"
+                width={50}
+                height={50}
+                className="rounded-xl border h-fit mt-2"
+              />
+              <div>
+                <p>
+                  <span className="text-primary font-bold">
+                    {item.username}
+                  </span>{" "}
+                  as just uploaded new items:{" "}
+                  <span className="text-primary font-bold">{item.items}</span>{" "}
+                  and more.
+                </p>
+                <p className="text-[#797979] mt-1">
+                  <span>{item.time}</span> ago
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Notifications;
