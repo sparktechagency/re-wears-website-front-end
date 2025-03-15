aa085567325ac8026dbf8ac658a58bf2b71470ef
Rahad Ullah <myemail.abc20@gmail.com>
2025-03-15 14:12:00 +0600

    Added contact us page

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -2,7 +2,9 @@
 
 interface InputProps {
   className?: string;
+  type?: string;
   placeholder?: string;
+  required?: boolean;
   defaultValue?: string | number;
   value?: string | number;
   setValue?: (e: string | number | undefined) => void;
@@ -10,13 +12,17 @@ interface InputProps {
 
 const Input = ({
   className,
+  type,
   placeholder,
+  required,
   defaultValue,
   value,
   setValue,
 }: InputProps) => {
   return (
     <input
+      required={required}
+      type={type}
       value={value}
       defaultValue={defaultValue}
       onChange={(e) => setValue && setValue(e.target.value)}
