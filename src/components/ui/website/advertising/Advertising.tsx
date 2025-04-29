f53e482d85c4b13f35f6be6a8320af26d27eecaf
Rahad Ullah <myemail.abc20@gmail.com>
2025-04-29 09:30:08 +0600

    Fixed issue

--- src/components/ui/website/advertising/Advertising.tsx
+++ src/components/ui/website/advertising/Advertising.tsx
@@ -5,7 +5,7 @@ const Advertising = () => {
   return (
     <div className="py-12 lg:py-20">
       <section className="container grid grid-cols-1 md:grid-cols-2 items-center gap-8">
-        <div>
+        <div className="grid gap-4 lg:gap-6">
           <h1 className="text-xl md:text-2xl lg:text-4xl font-bold">
             Advertise with re-wears
           </h1>
