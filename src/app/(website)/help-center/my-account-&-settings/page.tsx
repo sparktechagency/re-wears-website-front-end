bbae2a6fccc11a6a91b55d9bf8c7c7c90d748656
Rahad Ullah <myemail.abc20@gmail.com>
2025-06-28 22:29:53 +0600

    Help center content implemented

--- src/app/(website)/help-center/my-account-&-settings/page.tsx
+++ src/app/(website)/help-center/my-account-&-settings/page.tsx
@@ -1,6 +1,12 @@
 import BackButton from "@/components/shared/BackButton";
+import { myFetch } from "@/helpers/myFetch";
 
-const MyAccountAndSettingPage = () => {
+const MyAccountAndSettingPage = async () => {
+  const res = await myFetch(`/cms/my-account-settings`, {
+    cache: "no-store",
+  });
+  const content = res?.data?.content;
+
   return (
     <div>
       <section className="flex items-center gap-6">
@@ -11,39 +17,10 @@ const MyAccountAndSettingPage = () => {
       </section>
 
       {/* content section */}
-      <section className="grid gap-4 py-4">
-        <div className="flex gap-2">
-          <span className="font-bold">1.</span>
-          <p>
-            <span className="font-bold">Profile Management: </span>
-            Easily update your email, manage profile details, and change your
-            password by accessing Account Settings within your profile.
-          </p>
-        </div>
-        <div className="flex gap-2">
-          <span className="font-bold">2.</span>
-          <p>
-            <span className="font-bold">Username Changes: </span>
-            We permit username changes only in specific cases, such as
-            displaying personal information or under special circumstances. Each
-            request is reviewed individually to prevent members from losing
-            contact with each other or losing track of orders due to username
-            changes.
-          </p>
-        </div>
-        <div className="flex gap-2">
-          <span className="font-bold">3.</span>
-          <p>
-            <span className="font-bold">Need Help Logging In? </span>
-            If you&apos;re experiencing login issues or any other unlisted
-            problem, reach out to us at{" "}
-            <span className="text-primary font-bold">
-              support@re-wears.com{" "}
-            </span>{" "}
-            for assistance. We&apos;re here to help!
-          </p>
-        </div>
-      </section>
+      <article
+        dangerouslySetInnerHTML={{ __html: content }}
+        className="prose lg:prose-lg"
+      ></article>
     </div>
   );
 };
